5a0d5f2cc8a48d331ebf9eddbaca8001cecce914
Swati Vernwal <barnwalswati1997@gmail.com>
2024-08-11 13:32:17 +0530

    Revert "Added class"
    This reverts commit 2d0020268bbe93eef0eb582fabc123eef926de8e.

--- kafka-basics/src/main/java/io/demos/ConsumerDemo.java
+++ kafka-basics/src/main/java/io/demos/ConsumerDemo.java
@@ -11,7 +11,6 @@ import java.time.Duration;
 import java.util.List;
 import java.util.Properties;
 
-//started to learn kafka
 
 /**
  * how to code for Kafka-Consumer
--- kafka-basics/src/main/java/io/demos/NewClass.java
+++
@@ -1,4 +0,0 @@
-package io.demos;
-
-public class NewClass {
-}
